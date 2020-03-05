b7e51570f7aa6d1142f620ea06808ce9a99fd09e
Sossie <email>
2020-03-05 13:31:03 +0300

    Even numbers

---
+++ Evennumbers.py
@@ -0,0 +1,10 @@
+#Write a program to print the even numbers from given list [1,2,3,4,5,6,7,8,9]
+#Expected results:[2,4,6,8]
+
+def is_even_num(l):
+    enum = []
+    for n in l:
+        if n % 2 == 0:
+            enum.append(n)
+    return enum
+print(is_even_num([1, 2, 3, 4, 5, 6, 7, 8, 9]))
